10f0d5b248d1879211d5493f7bdda11edb7849a2
AADHIASARANA T <aadhiasarana12@gmail.com>
2026-06-13 21:36:38 +0530

    again

--- portfolio-v2/src/App.tsx
+++ portfolio-v2/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion } from 'framer-motion'
 import Navigation from './components/Navigation.tsx'
 import Hero from './components/Hero.tsx'
 import About from './components/About.tsx'
