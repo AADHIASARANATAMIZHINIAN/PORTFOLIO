0361450718f9cc59310c0adae44ef25d59f13912
AADHIASARANA T <aadhiasarana12@gmail.com>
2026-08-26 17:43:56 +0530

    new#1

--- 3d keyboard/Bf9ZJeOYJ9v-1772890343655/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { KeyboardScene } from "@/components/keyboard-scene"
-
-export default function Page() {
-  return (
-    <main className="w-full h-screen bg-neutral-950 overflow-hidden flex flex-col">
-      <KeyboardScene />
-    </main>
-  )
-}
